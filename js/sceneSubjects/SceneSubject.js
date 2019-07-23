6ea5b9d4376e89df744c47492b3ff01313a75099
Michela Federico <michela.federico@realisticgames.co.uk>
2019-07-23 09:38:55 +0100

    improving instances by using points

--- js/sceneSubjects/SceneSubject.js
+++ js/sceneSubjects/SceneSubject.js
@@ -1,66 +1,42 @@
 function SceneSubject(scene) {
-  var geometry = new THREE.CircleBufferGeometry(5, 32);
-  var texture = new THREE.TextureLoader().load( "images/textures/circle.png" );
-  var uniforms = {
-		pointTexture: { value: texture }
-	};
-  var material = new THREE.MeshBasicMaterial({
-    map: texture,
-    blending: THREE.AdditiveBlending,
-		transparent: true,
-    depthTest: true,
-  });
-  var shaderMaterial = new THREE.ShaderMaterial(
-  {
-		uniforms: uniforms,
-		vertexShader: getVertexShader(),
-		fragmentShader: getFragmentShader(),
-		blending: THREE.AdditiveBlending,
-		// depthTest: false,
-		transparent: true,
-		vertexColors: true
-	} );
-  var direction = [-1, 1];
-  var group = new THREE.Group();
-  var particles = 500;
-  var i, mesh;
-  var incrementalFactor = 0.1;
+	var geometry = new THREE.BufferGeometry();
+	var vertices = [];
+	var sprite = new THREE.TextureLoader().load( 'images/textures/circle.png' );
+	var numberOfParticles = 500;
+	var i, x, y, z;
+	var particles;
 
-	for ( i = 0; i < particles; i ++ )
-  {
-		mesh = new THREE.Mesh( geometry, material );
+	for ( i=0; i<numberOfParticles; i++ )
+	{
+		x = 2000 * Math.random() - 1000;
+		y = 2000 * Math.random() - 1000;
+		z = 2000 * Math.random() - 1000;
 
-    mesh.name = "mesh_"+i;
-		mesh.position.x = Math.random() * 200 - 100;//getRandomNumber(0, boundaries.x);
-		mesh.position.y = Math.random() * 200 - 100;//getRandomNumber(0, boundaries.y);
-		mesh.position.z = Math.random() * 200 - 100;//getRandomNumber(0, boundaries.z);
-		mesh.matrixAutoUpdate = true;
-		mesh.updateMatrix();
-		group.add( mesh );
+		vertices.push( x, y, z );
 	}
-	scene.add( group );
+	geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
+	material = new THREE.PointsMaterial( {
+		size: 50,
+		sizeAttenuation: false,
+		map: sprite,
+		alphaTest: 0.5,
+		transparent: true,
+		blending: THREE.AdditiveBlending
+	} );
 
-	this.update = function(){
-    for (i=0; i<particles; i++)
-    {
-      mesh = group.getObjectByName("mesh_"+i);
+	particles = new THREE.Points( geometry, material );
 
-      // mesh.position.x += incrementalFactor;
-  		mesh.position.y += incrementalFactor
-  		// mesh.position.z += incrementalFactor;
+	scene.add( particles );
 
-      // if (mesh.position.x > window.innerWidth/2)
-      // {
-      //   mesh.position.x =  Math.random() * 100 - 200;
-      // }
-      if (mesh.position.y > window.innerHeight/3)
-      {
-        mesh.position.y = Math.random() * 200 - 100;
-      }
-      // if (mesh.position.z > boundaries.z)
-      // {
-      //   mesh.position.z = Math.random() * 200 - 100;
-      // }
-    }
-  };
+	this.update = function() {
+		var positions = particles.geometry.attributes.position.array;
+
+		for ( i=1; i<numberOfParticles*3; i+=3 )
+		{
+			positions[i] += 0.1 + Math.random();
+		}
+
+		particles.geometry.attributes.position.needsUpdate = true;
+		particles.geometry.setDrawRange( 0, numberOfParticles );
+	}
 }
--- js/utils.js
+++ js/utils.js
@@ -3,21 +3,21 @@ function getRandomNumber(min, max) {
 }
 
 function getVertexShader() {
-    return `attribute float size;
-			varying vec3 vColor;
-			void main() {
-				vColor = color;
-				vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-				gl_PointSize = size * ( 300.0 / -mvPosition.z );
-				gl_Position = projectionMatrix * mvPosition;
-			}`
+    return `attribute vec3 aColor;
+            varying vec3 vColor;
+
+            void main() {
+              vColor = aColor;
+        
+              vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+              gl_Position = projectionMatrix * modelViewPosition; 
+            }`
 }
 
 function getFragmentShader() {
-  return `uniform sampler2D pointTexture;
-			varying vec3 vColor;
-			void main() {
-				gl_FragColor = vec4( vColor, 1.0 );
-				gl_FragColor = gl_FragColor * texture2D( pointTexture, gl_PointCoord );
-			}`
+  return `varying vec3 vColor;
+
+          void main() {
+            gl_FragColor = vec4(vColor, 1);
+          }`
 }
